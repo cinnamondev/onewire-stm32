c1279eb119796d7ccf71ec9d13aceca02eb62a27
cinnamondev <justpromptiguess@gmail.com>
2024-02-13 17:03:07 +0000

    work in progress basic onewire c style

--- OneWire.cpp
+++ OneWire.cpp
@@ -1,21 +1,59 @@
 #include "OneWire.hpp"
+#include "mbed.h"
+#ifdef DEBUG
+static bool isOneInstance = false; // debug marker multiple instances started
+#endif
+static struct onewire_iface iface;
 
-OneWire(USART_TypeDef *usart, uint32_t baud) {
-    usart.Instance = iface;
-    usart.Init.BaudRate = 9600;
-    usart.Init.WordLength = UART_WORDLENGTH_8B;
-    usart.Init.StopBits = UART_STOPBITS_1;
-    usart.Init.Parity = UART_PARITY_NONE;
-    usart.Init.Mode = UART_MODE_TX_RX;
-    usart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-    usart.Init.OverSampling = UART_OVERSAMPLING_16;
-    if (HAL_HalfDuplex_Init(&usart) != HAL_OK)
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef* uart) {
+    if(iface.bus.Instance != uart) return;
+    if(iface.active_cb_rx) iface.active_cb_rx();
+    if(iface.inflight_rx_buffer != NULL) {
+        iface.inflight_rx_buffer = NULL;
+        iface.inflight_rx_buffer_size = 0;
+    }
+    iface.active_cb_rx = 0;
+}
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef* uart) {
+    if (iface.active_cb_tx) iface.active_cb_tx();
+    if (iface.inflight_rx_buffer != NULL) {
+        onewire_read(iface.inflight_rx_buffer, iface.inflight_rx_buffer_size, iface.active_cb_rx);
+    }
+    iface.active_cb_tx = 0;
+}
+
+void onewire_init(USART_TypeDef* usart, unsigned long baud) {
+#ifdef DEBUG
+    if(isOneInstance) {
+        printf("Mult OW Inst N/A. Will break RELEASE.");
+    }
+#endif
+    iface.bus.Instance = usart;
+    iface.bus.Init.BaudRate = 9600;
+    iface.bus.Init.WordLength = UART_WORDLENGTH_8B;
+    iface.bus.Init.StopBits = UART_STOPBITS_1;
+    iface.bus.Init.Parity = UART_PARITY_NONE;
+    iface.bus.Init.Mode = UART_MODE_TX_RX;
+    iface.bus.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    iface.bus.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_HalfDuplex_Init(&iface.bus) != HAL_OK)
     {
 #ifdef DEBUG
 #endif
-        OneWire_Error_Handler();
     }
-    return &usart;
 }
 
+// Base methods
+void onewire_write_read_blk() {
+
+}
+void onewire_write_blk() {}
+void onewire_write() {}
+void onewire_read_blk(uint8_t buff, size_t n) {}
+void onewire_read(uint8_t* buff, size_t n, Callback<void()> cb_fin) {}
+void onewire_write_read(uint8_t* tx_buff, size_t tx_n, uint8_t* rx_buf, size_t rx_n, Callback<void()> cb_fin) {
+    iface.active_cb_rx = cb_fin;
+    HAL_UART_Transmit_IT(&iface.bus, tx_buff, tx_n);
+    HAL_UART_Receive_IT();
 }
--- OneWire.hpp
+++ OneWire.hpp
@@ -1,8 +1,40 @@
-#include "stm32f4xx_hal_usart.h"
+#include "stm32f4xx_hal_uart.h"
+#include "mbed.h"
 
-class OneWire {
-    protected:
-    UART_HandleTypeDef usart;
-    OneWire(USART_TypeDef* usart, uint32_t baud);
+struct onewire_iface {
+    Mutex lock;
+    UART_HandleTypeDef bus;
+    Callback<void()> active_cb_rx = 0;
+    Callback<void()> active_cb_tx = 0;
+    uint8_t* inflight_rx_buffer = NULL;
+    size_t inflight_rx_buffer_size = 0;
+};
 
-}
+/**
+ * @brief Write bytes to OneWire bus (blocking)
+ * 
+ */
+void onewire_write_blk();
+/**
+ * @brief Write bytes to OneWire Bus (non-blocking)
+ * 
+ */
+void onewire_write();
+void onewire_read_blk(uint8_t buff, size_t n);
+void onewire_read(uint8_t* buff, size_t n, Callback<void()> cb_fin);
+// Internally, onewire_read_blind will not remove callbacks, but it will
+// not intentionally set them. States that use callbacks will generally clear them once
+// they are done, so this can be used in edge cases where you do not need to update the callback
+// (edge cases defined in HAL_UART_{RX/TX}CpltCallback)
+
+void onewire_write_read_blk();
+/**
+ * @brief Write & Read bytes from OneWire bus (Non-blocking)
+ * 
+ * @param tx_buff 
+ * @param tx_n 
+ * @param rx_buf 
+ * @param rx_n 
+ * @param cb_fin 
+ */
+void onewire_write_read(uint8_t* tx_buff, size_t tx_n, uint8_t* rx_buf, size_t rx_n, Callback<void()> cb_fin);
